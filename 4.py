fd42223672bca6a70d10bf4c88238634e577c8fa
Bartłomiej <b.allaburda@gmail.com>
2024-10-16 20:42:16 +0200

    Exercise 4 done

--- 4.py
+++ 4.py
@@ -1,40 +1,48 @@
+from sklearn import datasets
+from sklearn.decomposition import PCA
+import pandas as pd
 import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d
-import numpy as np
-from sklearn import datasets, decomposition
-
-np.random.seed(5)
 
 iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+X = pd.DataFrame(iris.data, columns=iris.feature_names)
+y = pd.Series(iris.target, name='FlowerType')
+print(X.head())
+# pca_iris = PCA(n_components=3).fit(iris.data)
+pca_iris = PCA(n_components=2).fit(iris.data)
+# pca_iris = PCA(n_components=1).fit(iris.data)
+# pca_iris = PCA(n_components=4).fit(iris.data)
+print("Used PCA:")
+print(pca_iris)
+print("PCA variance ratio:")
+print(pca_iris.explained_variance_ratio_)
+print("PCA components:")
+print(pca_iris.components_)
+print("Dataset after transform:")
+print(pca_iris.transform(iris.data))
 
-fig = plt.figure(1, figsize=(4, 3))
-plt.clf()
+# Aby zachować minimum 95% wariancji z datasetu iris można usunąc 2 kolumny.
+# Wariancja dla 4 kolumn [0.92461872 0.05306648 0.01710261 0.00521218] wynosi: 1
+# Wariancja dla 3 kolumn [0.92461872 0.05306648 0.01710261] wynosi około: 0.92 + 0.05 + 0.02 = 0.99 - 1% straty informacji
+# Wariancja dla 2 kolumn [0.92461872 0.05306648] wynosi około: 0.92 + 0.05 = 0.97 - 3% straty informacji
+# Wariancja dla 1 kolumny [0.92461872] wynosi około: 0.92  - 8% straty informacji
+# Jak widać dla 3 kolumn strata informacji strata informacji jest znikoma na poziome 1%, więc można usunąc kolejną
+# kolumna. Rozważając usunięcie 3 kolumn strata informacji wyniesie 8% co przekracza założenia.
+# Sumaryczna wariancja dla wszystkich komponentów = 1 - [0.92461872 0.05306648 0.01710261 0.00521218]
+# Strata informacji dla:
+#   - 1 usuniętej kolumny = 0.00521218
+#   - 2 usuniętych kolumn = 0.01710261 + 0.00521218 = 0.022313828
+#   - 3 usuniętych kolumn = 0.05306648 + 0.01710261 + 0.00521218 = 0.075380308
 
-ax = fig.add_subplot(111, projection="3d", elev=48, azim=134)
-ax.set_position([0, 0, 0.95, 1])
-
-
-plt.cla()
-pca = decomposition.PCA(n_components=3)
-pca.fit(X)
-X = pca.transform(X)
-
-for name, label in [("Setosa", 0), ("Versicolour", 1), ("Virginica", 2)]:
-    ax.text3D(
-        X[y == label, 0].mean(),
-        X[y == label, 1].mean() + 1.5,
-        X[y == label, 2].mean(),
-        name,
-        horizontalalignment="center",
-        bbox=dict(alpha=0.5, edgecolor="w", facecolor="w"),
-    )
-# Reorder the labels to have colors matching the cluster results
-y = np.choose(y, [1, 2, 0]).astype(float)
-ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.nipy_spectral, edgecolor="k")
-
-ax.xaxis.set_ticklabels([])
-ax.yaxis.set_ticklabels([])
-ax.zaxis.set_ticklabels([])
+# Plot
+iris_df = pd.DataFrame(pca_iris.transform(iris.data), columns=['PC1', 'PC2'])
+iris_df['FlowerType'] = iris.target
+plt.figure(figsize=(8, 6))
+colors = ['blue', 'cyan', 'orange']
+labels = ['Setosa', 'Versicolor', 'Virginica']
+for i, color, label in zip([0, 1, 2], colors, labels):
+    subset = iris_df[iris_df['FlowerType'] == i]
+    plt.scatter(subset['PC1'], subset['PC2'], c=color, label=label, s=50)
+plt.title("PCA of IRIS dataset")
+plt.legend()
+plt.grid(True)
 plt.show()
