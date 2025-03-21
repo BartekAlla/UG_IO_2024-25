610fa1192cd1a3dfe0185e131862864f16083e0f
Bartłomiej <b.allaburda@gmail.com>
2025-03-21 07:19:19 +0100

    update

--- bagofwords_connotations_recursivenetworks/lstm03.py
+++ bagofwords_connotations_recursivenetworks/lstm03.py
@@ -43,7 +43,7 @@ model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 # load the network weights
-filename = "../weights-improvement-02-2.6100.keras"
+filename = "../models/bagofwords/weights-improvement-02-2.6100.keras"
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # pick a random seed
